1a89ff9a4ae85e64592c88d78b32c45c61df0c3c
White3ugar <lew20021014@gmail.com>
2025-05-19 16:53:54 +0800

    UpdatedFiles

--- android/build/build/generated/source/buildConfig/mono/debug/com/godot/game/BuildConfig.java
+++ android/build/build/generated/source/buildConfig/mono/debug/com/godot/game/BuildConfig.java
@@ -5,7 +5,7 @@ package com.godot.game;
 
 public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
-  public static final String APPLICATION_ID = "com.example.PICOProject";
+  public static final String APPLICATION_ID = "com.example.MagicalBalloonADventure";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "mono";
   public static final int VERSION_CODE = 1;
